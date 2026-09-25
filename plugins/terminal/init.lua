a3c98844206407752d8e3509f0338e6284572bd0
mlgudi <me@gudi.tube>
2026-09-25 08:13:39 +0100

    Fix newline for non-cmd shell on Windows

--- plugins/terminal/init.lua
+++ plugins/terminal/init.lua
@@ -269,6 +269,7 @@ function TerminalView:supports_text_input() return true end
 function TerminalView:new(options)
   TerminalView.super.new(self)
   options = common.merge(common.merge({}, config.plugins.terminal), options)
+  options.newline = options.shell:find("cmd.exe") and "\r\n" or "\r"
   self.size.y = options.drawer_height
   self.options = options
   self.options.environment = common.merge(options.environment, {})
